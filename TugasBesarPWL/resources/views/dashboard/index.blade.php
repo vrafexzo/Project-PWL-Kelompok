ca190b989fdce2217c95b1ee6182a5a11d1c6af2
Joni Hendrawan <113569553+JONIHENDRAWAN@users.noreply.github.com>
2024-04-10 23:37:53 +0700

    Mengubah sedikit tampilan dari halaman dashboard

--- TugasBesarPWL/resources/views/dashboard/index.blade.php
+++ TugasBesarPWL/resources/views/dashboard/index.blade.php
@@ -2,9 +2,21 @@
 
 @section('container')
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Polling</h1>
+    <h1 class="h2">Polling Mata Kuliah</h1>
   </div> 
-  <h2 class="index">kapan?</h2>
-  <h2 class="index">Jika sudah buka tombol href ke pemilihan sks</h2>
+  <div class="perwalian-logo">
+    <img src="/img/perwalian.png" alt="">
+  </div>
+  <div class="list-dashboard">
+    <div class="padding-list">
+      <h6>Gagal polling dapat dikarenakan:</h6>
+      <ul>
+          <li>Di luar jadwal yang ditentukan</li>
+          <li>Kewajiban keuangan mahasiswa belum terpenuhi</li>
+          <li>Tidak terdaftar sebagai mahasiswa aktif</li>
+          <li>Belum mengembalikan pinjaman buku</li>
+      </ul>  
+    </div>
+  </div>
 
 @endsection
